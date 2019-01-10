a23d6afa1ff4492480d77c93b41259acb3470613
twerner1 <44643999+twerner1@users.noreply.github.com>
2019-01-10 16:05:47 -0600

    Update ExtendedMath.java

--- src/ExtendedMath.java
+++ src/ExtendedMath.java
@@ -1,9 +1,17 @@
 public class ExtendedMath {
-    // TODO: implement this
-    public static float normalizeRadians(float radians) {
-        // should take in any number of radians, and output the equivalent in the range [-pi, pi]
-        // do NOT use a loop.
-        // hint: maybe make another function called normalizeRadians2pi which normalizes to [0, 2pi] first
-        return 0;
+  public static float normalizeRadians(float radians) {
+    // should take in any number of radians, and output the
+    //equivalent in the range [-pi, pi]
+    // do NOT use a loop.
+    // hint: maybe make another function called normalizeRadians2pi which normalizes to [0, 2pi] first
+    if(radians < -1){
+    	return (radians * -1) - 1;
+  	}
+   	else if(radians > 1){
+   		return 1 - ((radians * -1) % 1);
     }
+   	else{
+    	return radians;
+    }
+  }
 }
