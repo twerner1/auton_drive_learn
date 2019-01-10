d794b078165c73453377d006cd992378c13195a8
twerner1 <44643999+twerner1@users.noreply.github.com>
2019-01-10 16:06:11 -0600

    Update Robot.java

--- src/Robot.java
+++ src/Robot.java
@@ -6,10 +6,12 @@ public class Robot {
     public void moveTo(float x, float y, float heading) {
         moveTo(new Vector2F(x, y), heading);
     }
-
+    //to find the vector sub yours and target
     // TODO: implement this
     public void moveTo(Vector2F targetPosition, float targetHeading) {
         // move to the given position and heading
         // you should only have to use things that are within these files
+        rotate(normalizeRadians(targetPosition) - getHeading());
+        forwardDrive(subtract(targetPosition).getMagnitude());
     }
 }
